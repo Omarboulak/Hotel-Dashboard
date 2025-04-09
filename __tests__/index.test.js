7a7445f5f4f83f8ee3384f05f7794836a9f5079e
Omar <omarboulakchour@gmail.com>
2025-04-09 09:52:18 +0200

    añadido dependencias de jest

---
+++ __tests__/index.test.js
@@ -0,0 +1,89 @@
+import Room from "../src/class/rooms";
+import Booking from "../src/class/booking";
+
+describe('propiedades de la clase de Rooms', () =>{
+    it('El nombre introducido es un string', () =>{
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(typeof room.name).toBe('string')
+    })
+
+    it('Los Booking tiene que ser un array', () => {
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(Array.isArray(room.Booking)).toBe(true);
+    })
+
+    it('El rate tiene que ser un numero', () => {
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(typeof room.rate).toBe('number');
+        expect(room.rate).toBeGreaterThanOrEqual(0);
+    })
+
+    it('El discount tiene que ser un nuemero del 0 al 100', () => {
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(room.discount).toBeGreaterThanOrEqual(0);
+        expect(room.discount).toBeLessThanOrEqual(100);
+    })
+
+    it('el metodo isOccupied tiene que ser booleano', () => {
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(typeof room.isOccupied('12/12/2025')).toBe('boolean');
+    })
+
+    it('el metodo occupancyPercentage tiene que devolver un numero', () => {
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(typeof room.occupancyPercentage('12/12/2025', '12/15/2025')).toBe('number');
+    })
+
+    it('el metodo totalOccupancyPercentage tiene que devolver un numero', () => {
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(room.totalOccupancyPercentage('double', '12/12/2025', '12/15/2025')).toBeGreaterThanOrEqual(0);
+    })
+
+    it('el metodo availableRooms tiene que devolver un numero', () => {
+        const booking = new Booking();
+        const room = new Room('double', Booking, 100, 10);
+        expect(Array.isArray(room.availableRooms('double', '12/12/2025', '12/15/2025'))).toBe('array');
+    })
+})
+
+
+describe('propiedades de la clase de Booking', () =>{
+    it('El nombre introducido de booking tiene que ser un string', () =>{
+        const room = new Room('double', Booking, 100, 10);
+        const booking = new Booking('Omar', 'omarboulakchour@gmail.com', '12/12/2025', '12/15/2025', 10);
+        expect(typeof Booking.name).toBe('string')
+    })
+
+    it('El empail introducido de booking tiene que ser un string', () =>{
+        const room = new Room('double', Booking, 100, 10);
+        const booking = new Booking('Omar', 'omarboulakchour@gmail.com', '12/12/2025', '12/15/2025', 10);
+        expect(typeof Booking.email).toBe('string')
+    })
+
+    it('El checkIn introducido tiene que ser un date', () =>{
+        const room = new Room('double', Booking, 100, 10);
+        const booking = new Booking('Omar', 'omarboulakchour@gmail.com', '12/12/2025', '12/15/2025', 10);
+        expect(Booking.checkIn instanceof Date).toBe(true)
+    })
+    
+    it('El checkOut introducido tiene que ser un date', () =>{
+        const room = new Room('double', Booking, 100, 10);
+        const booking = new Booking('Omar', 'omarboulakchour@gmail.com', '12/12/2025', '12/15/2025', 10);
+        expect(Booking.checkOut instanceof Date).toBe(true)
+    })
+
+    it('El discount de Booking introducido tiene que ser un numero', () =>{
+        const room = new Room('double', Booking, 100, 10);
+        const booking = new Booking('Omar', 'omarboulakchour@gmail.com', '12/12/2025', '12/15/2025', 10);
+        expect(typeof Booking.discount).toBe('number')
+    })
+
+
+})
--- src/class/rooms.js
+++ src/class/rooms.js
@@ -1,15 +1,18 @@
 class Room {
-    constructor(name, bookings, rate, discount) {
-      this.name = name;
-      this.bookings = bookings;
-      this.rate = rate;
-      this.discount = discount;
+  constructor(name, bookings, rate, discount) {
+    this.name = name;
+    this.bookings = bookings;
+    this.rate = rate;
+    this.discount = discount;
+  }
+
+  isOccupied(date) {
+    if (!(date instanceof Date)) {
+      throw new Error('los datos introducidos tienen que ser de tipo Date');
     }
-  
-    isOccupied(date) {
-      if (!(date instanceof Date)) {
-        throw new Error('los datos introducidos tienen que ser de tipo Date');
-      }
-      return this.bookings.some(booking => date >= booking.checkIn && date < booking.checkOut);
-    } 
+    return this.bookings.some(booking => date >= booking.checkIn && date < booking.checkOut);
+  }
 }
+
+
+export default Room;
