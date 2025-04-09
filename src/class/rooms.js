35a291b67922ba2636ce3a1fa7995d069a5b7360
Omar <omarboulakchour@gmail.com>
2025-04-09 09:09:50 +0200

    Class Room

--- src/class/rooms.js
+++ src/class/rooms.js
@@ -1,17 +1,5 @@
 class Room {
     constructor(name, bookings, rate, discount) {
-      if (typeof name !== 'string') {
-        throw new Error('Name must be a string');
-      }
-      if (!Array.isArray(bookings)) {
-        throw new Error('Bookings must be an array');
-      }
-      if (typeof rate !== 'number') {
-        throw new Error('Rate must be a number');
-      }
-      if (typeof discount !== 'number') {
-        throw new Error('Discount must be a number');
-      }
       this.name = name;
       this.bookings = bookings;
       this.rate = rate;
